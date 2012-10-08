13dc299b5864a84d3ef497dd3d7c5b855ac609f0
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2012-10-08 05:52:08 +0000

    allow selection of parent nodes for picking all children of that ndoe (maybe change later as it performs an implicit change to a data structure which wont be immediately apparent)

--- nova/cells/filters/pick_cell_filter.py
+++ nova/cells/filters/pick_cell_filter.py
@@ -8,12 +8,21 @@ class PickCellFilter(BaseCellFilter):
         LOG.info(_("Filtering cells for a specific cell name"))
         scheduler_hints = filter_properties.get('scheduler_hints', None)
         cell_name = scheduler_hints.get('use_cell', None)
+        if not cell_name:
+            return {}
+        # remove this as once the call reaches the 
+        # specified top cell, we want to proceed
+        # using normal scheduling
+        scheduler_hints.pop('use_cell')
+        
+        # FIXME: cell name will come in hyphen separated
+        # from the CLI as bangs break it. This should be
+        # massaged out at a higher level
         cell_name = cell_name.replace('-', '!')
-        if cell_name:
-            LOG.info(_("Filtering for cell '%(cell_name)s'"), locals())
-            resp = {
-                'action': 'direct_route',
-                'target': cell_name,
-                }
-            return resp
-        return []
+
+        LOG.info(_("Filtering for cell '%(cell_name)s'"), locals())
+        resp = {
+            'action': 'direct_route',
+            'target': cell_name,
+            }
+        return resp
