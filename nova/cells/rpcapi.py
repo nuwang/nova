e55f0c7d74d6d18265884308b55c58b08a848ce6
Sam Morrison <sorrison@gmail.com>
2013-06-05 09:56:34 +1000

    Support consoleauth at child cells
    Conflicts:
    
    	nova/cells/manager.py
    	nova/cells/messaging.py
    	nova/cells/rpcapi.py

--- nova/cells/rpcapi.py
+++ nova/cells/rpcapi.py
@@ -328,3 +328,15 @@ class CellsAPI(rpc_proxy.RpcProxy):
                               aggregate_id=aggregate_id,
                               host_name=host_name),
                 version='1.6.1')
+
+    def authorize_console(self, ctxt, cell_name, token, console_type, host, port,
+                          internal_access_path, instance_uuid=None):
+        # The remote side doesn't return anything, but we want to block
+        # until it completes.
+        return self.call(ctxt,
+                self.make_msg('authorize_console', cell_name=cell_name,
+                              token=token, console_type=console_type,
+                              host=host, port=port,
+                              internal_access_path=internal_access_path,
+                              instance_uuid=instance_uuid),
+                version="1.6.1")
