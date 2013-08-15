6cba67f6fdcbfcb73219c5684194a87faa403abb
Kieran Spear <kispear@gmail.com>
2013-08-15 15:52:49 +1000

    Expire reservations in cells manager
    Normally reservations are expired by a periodic task in nova-scheduler.
    In a cells deployment quotas are usually enabled only on the api-level
    cell. This uses a separate DB from the scheduler, so any reservations
    on the top level never get expired.
    
    This change adds the same periodic task to the cells manager so
    reservations at all levels have a chance to be expired. The task is
    disabled by default and can be turned on with the
    CONF.cells.expire_reservations option.
    
    Change-Id: I217f5b5e947662a858eb295f2add5c3227c3b531
    (cherry picked from commit b4d902db45f7dbedfe57e56e6bbbb5d782f0626d)

--- nova/cells/manager.py
+++ nova/cells/manager.py
@@ -31,6 +31,7 @@ from nova import manager
 from nova.openstack.common import importutils
 from nova.openstack.common import periodic_task
 from nova.openstack.common import timeutils
+from nova import quota
 
 cell_manager_opts = [
         cfg.StrOpt('driver',
@@ -42,13 +43,19 @@ cell_manager_opts = [
                         "or deleted to continue to update cells"),
         cfg.IntOpt("instance_update_num_instances",
                 default=1,
-                help="Number of instances to update per periodic task run")
+                help="Number of instances to update per periodic task run"),
+        cfg.BoolOpt("expire_reservations",
+                default=False,
+                help="If True, enable the periodic task for reservation "
+                     "expiry."),
 ]
 
 
 CONF = cfg.CONF
 CONF.import_opt('name', 'nova.cells.opts', group='cells')
 CONF.register_opts(cell_manager_opts, group='cells')
+
+QUOTAS = quota.QUOTAS
 
 
 class CellsManager(manager.Manager):
@@ -651,6 +658,11 @@ class CellsManager(manager.Manager):
             return
         handler = self.consistency_handlers[resource_name]
         handler.heal_entries(ctxt)
+
+    @periodic_task.periodic_task
+    def _expire_reservations(self, context):
+        if CONF.cells.expire_reservations:
+            QUOTAS.expire(context)
 
     @periodic_task.periodic_task
     def _heal_security_groups(self, ctxt):
