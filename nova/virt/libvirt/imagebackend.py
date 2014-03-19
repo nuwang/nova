ed0942a0a997ebaeb508f8db45968cffa1747363
Sam Morrison <sorrison@gmail.com>
2014-03-19 16:07:20 +1100

    Fix wrong disk factor in RBD
    Change-Id: Ieae1ba61c2de21e9764ca2278496eb0374bf762c

--- nova/virt/libvirt/imagebackend.py
+++ nova/virt/libvirt/imagebackend.py
@@ -666,7 +666,7 @@ class Rbd(Image):
         return False
 
     def _resize(self, volume_name, size):
-        size = int(size) * 1024
+        size = int(size)
 
         with RBDVolumeProxy(self, volume_name) as vol:
             vol.resize(size)
