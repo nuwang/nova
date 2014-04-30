864c43ec76939131750ada4a0a26d314c93fe16d
Sam Morrison <sorrison@gmail.com>
2014-04-30 15:23:19 +1000

    fixup! Use sysmetadata to get instance_name in API

--- nova/api/openstack/compute/contrib/extended_server_attributes.py
+++ nova/api/openstack/compute/contrib/extended_server_attributes.py
@@ -31,7 +31,7 @@ class ExtendedServerAttributesController(wsgi.Controller):
     def _get_hypervisor_instance_name(self, context, instance):
         if not CONF.cells.enable:
             return instance['name']
-        sys_metadata = utils.instance_meta(instance)
+        sys_metadata = utils.instance_sys_meta(instance)
         return sys_metadata.get('instance_name', '')
 
     def _extend_server(self, context, server, instance):
