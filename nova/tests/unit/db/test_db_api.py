4ca9d4bc414a13fb1059d52fdb52b23d389520bf
Russell Sim <russell.sim@gmail.com>
2014-06-03 17:05:56 +1000

    Nova API server read cell state from DB
    Store the cell capabilities updates in the DB.  Allow the Nova API to
    read the cell capabilities from the DB instead of via a RPC call.
    
    Change-Id: I66c824c3023b37240ce333dffab50542df9c34fa

--- nova/tests/unit/db/test_db_api.py
+++ nova/tests/unit/db/test_db_api.py
@@ -7105,6 +7105,7 @@ class CellTestCase(test.TestCase, ModelsObjectComparatorMixin):
             'weight_offset': 0.5,
             'weight_scale': 1.5,
             'is_parent': True,
+            'capabilities': '{}',
         }
 
     def _cell_value_modify(self, value, step):
@@ -7144,7 +7145,8 @@ class CellTestCase(test.TestCase, ModelsObjectComparatorMixin):
         test_cellname = self._get_cell_base_values()['name']
         updated_cell = db.cell_update(self.ctxt, test_cellname, new_values)
         self._assertEqualObjects(updated_cell, new_values,
-                                 ignored_keys=self._ignored_keys + ['name'])
+                                 ignored_keys=self._ignored_keys + ['name',
+                                                            'capabilities'])
 
     def test_cell_delete(self):
         new_cells = self._create_cells()
