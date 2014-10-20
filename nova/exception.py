aa14daf36d70728a4e98066b7fb8a1de895703bc
Sam Morrison <sorrison@gmail.com>
2014-10-20 14:04:58 +1100

    Print cell name in cell timeout exceptions
    Change-Id: I99fa337bd28142d74772bab63ac73f73f222a056

--- nova/exception.py
+++ nova/exception.py
@@ -970,7 +970,7 @@ class CellServiceAPIMethodNotFound(NotFound):
 
 
 class CellTimeout(NotFound):
-    msg_fmt = _("Timeout waiting for response from cell")
+    msg_fmt = _("Timeout waiting for response from cell %(cell_name)s")
 
 
 class CellMaxHopCountReached(NovaException):
