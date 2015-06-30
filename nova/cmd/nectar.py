4a0d49bb7163689ba4d44f066c50d13d73bfe6a3
Sam Morrison <sorrison@gmail.com>
2015-06-30 15:38:26 +1000

    Fix up nectar remove_bdm for Juno changes
    Change-Id: I49f8279dd5460774dcd96080def0beefd592e946

--- nova/cmd/nectar.py
+++ nova/cmd/nectar.py
@@ -1,9 +1,8 @@
 import socket
 
 from nova import context
-from nova.compute import manager
 from nova.conductor import rpcapi as conductor_rpcapi
-from nova.objects import block_device
+from nova.objects import block_device, instance as instance_obj
 
 
 # Decorators for actions
@@ -22,22 +21,16 @@ class NectarCommands(object):
     def remove_bdm(self, instance, volume):
         ctx = context.get_admin_context()
         c = conductor_rpcapi.ConductorAPI()
-        instance = c.instance_get_by_uuid(ctx, instance)
+        instance = instance_obj.Instance.get_by_uuid(ctx, instance)
+
         if instance['host'] != socket.gethostname():
             print("Instance not running on this host")
             return
         bdms = c.block_device_mapping_get_all_by_instance(ctx, instance)
-        compute_mgr = manager.ComputeManager(
-            compute_driver='nova.virt.libvirt.LibvirtDriver')
-        attached_disks = compute_mgr.driver.get_disks(instance['name'])
         for bdm in bdms:
             if bdm['volume_id'] == volume:
                 b = block_device.BlockDeviceMapping.get_by_volume_id(
                     ctx, volume, instance_uuid=instance['uuid'])
-                disk = b.device_name.split('/')[-1]
-                if disk in attached_disks:
-                    print("Not deleting, device attached to virt domain")
-                    return
 
                 print("Deleting BDM with ID %s" % b.id)
                 b.destroy()
