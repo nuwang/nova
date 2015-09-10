cbf73b8de90157f4a5ed2a6ba8abc0b4be1b6022
Sam Morrison <sorrison@gmail.com>
2015-09-10 13:06:11 +1000

    Revert "Don't check flavor disk size when booting from volume"
    This reverts commit 85ca946e836f5349911667cea2652297d7c21c06.
    
    Change-Id: I37698ef863f51c3e4498bc7536ce1b563868df81

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -672,7 +672,7 @@ class API(base.Base):
         # drivers. A value of 0 means don't check size.
         root_gb = instance_type['root_gb']
         if root_gb:
-            if image_id and int(image.get('size') or 0) > root_gb * (1024 ** 3):
+            if int(image.get('size') or 0) > root_gb * (1024 ** 3):
                 raise exception.FlavorDiskTooSmall()
 
             if int(image.get('min_disk') or 0) > root_gb:
